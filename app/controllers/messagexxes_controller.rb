e12b7dd87b3cc7c983ab000f2082aa21002e28ad
TatsuyaYokokawa <beattherich@iCloud.com>
2020-03-13 18:36:45 +0900

    レイアウト崩れ　対処
    &__type-a-message{　
    display flex 消した

--- app/controllers/messagexxes_controller.rb
+++ app/controllers/messagexxes_controller.rb
@@ -1,30 +1,30 @@
 class MessagexxesController < ApplicationController
-	before_action :set_group
+  before_action :set_group
 
-	def index
-	  @message = Messagexx.new
-	  @messages = @group.messagexxes.includes(:account)
-	end
-  
-	def create
-	  @message = @group.messagexxes.new(messagexx_params)
-	  if @message.save
-		redirect_to group_messagexxes_path(@group), notice: 'メッセージが送信されました'
-	  else
-		@messages = @group.messagexxes.includes(:account)
-		flash.now[:alert] = 'メッセージを入力してください。'
-		render :index
-	  end
-	end
-  
-	private
-  
-	def messagexx_params
-	  params.require(:messagexx).permit(:content, :image).merge(account_id: current_account.id)
-	end
-  
-	def set_group
-	  @group = Group.find(params[:group_id])
-	end
+  def index
+    @message = Messagexx.new
+    @messages = @group.messagexxes.includes(:account)
   end
   
+  def create
+    @message = @group.messagexxes.new(messagexx_params)
+    if @message.save
+      redirect_to group_messagexxes_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messagexxes.includes(:account)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+	private
+
+  def messagexx_params
+    params.require(:messagexx).permit(:content, :image).merge(account_id: current_account.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
+
