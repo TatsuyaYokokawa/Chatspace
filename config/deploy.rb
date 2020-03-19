00bb980fdf1df7514413ab8ca20e54cbe3c1699a
TatsuyaYokokawa <beattherich@iCloud.com>
2020-03-19 17:47:32 +0900

    本番環境

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.12.1"
 
 set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
+set :linked_files, %w{ config/secrets.yml }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -38,34 +39,35 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 lock '3.12.1'
-
-# Capistranoのログの表示に利用する
 set :application, 'Chatspace'
-
-# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:TatsuyaYokokawa/Chatspace.git'
-
-# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
-
-# どの公開鍵を利用してデプロイするか
+set :rbenv_ruby, '2.5.1' 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/harlequinarltoho.pem'] 
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
